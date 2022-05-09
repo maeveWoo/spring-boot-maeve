670d381fe14f0507501f00f36f000b5aa1a9e789
maeve <maeve.woo@kakao.com>
2022-05-09 16:04:27 +0900

    init

---
+++ src/test/java/io/springbootmaeve/SpringBootMaeveApplicationTests.java
@@ -0,0 +1,13 @@
+package io.springbootmaeve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMaeveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
